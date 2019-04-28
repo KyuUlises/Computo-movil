372d0576e0a99b3b480ebd9d17c1f8b9e5b9a590
Ulises Mendoza <EdUlisesMendoza@gmail.com>
2019-04-28 14:32:07 -0500

    Add files via upload

---
+++ EjClase/thortilla/thortilla/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  thortilla
+//
+//  Created by macbook on 4/22/19.
+//  Copyright © 2019 Sharon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
