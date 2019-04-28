13567db3a7665edc171ef2aecc0328d84201b7e0
Ulises Mendoza <EdUlisesMendoza@gmail.com>
2019-04-28 14:32:48 -0500

    Add files via upload

---
+++ EjClase/multiPantallas/multiPantallas/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  multiPantallas
+//
+//  Created by macbook on 4/1/19.
+//  Copyright © 2019 Sharon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue){
+        
+        
+    }
+
+}
+
